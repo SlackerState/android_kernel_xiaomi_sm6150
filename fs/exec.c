1fedbd729b2f03cdcfbb0bdd50657599e786ef75
SlackerState <slackerstate@gmail.com>
2023-02-06 22:51:16 +0800

    Implemented KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1715,6 +1715,8 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
 /*
  * sys_execve() executes a new program.
  */
@@ -1723,6 +1725,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
